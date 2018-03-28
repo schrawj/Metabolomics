efdee561c35c079bceb72161042de3eeffbbd9d9
Jeremy Schraw <schraw@bcm.edu>
2018-03-28 09:17:35 -0500

    New metabolites; backwards selection
    Changed one of the metabolties for MRD due to linear separation issues.  Updated both MRD and relapse code to provide backtransformed coeficients and 95% CIs for old models, as well as  for a parsimonious combined model chosen via stepwise backwards selection.

--- logistic regression - models.R
+++ logistic regression - models.R
@@ -12,10 +12,6 @@
 setwd('Y:/Jeremy Schraw/Metabolomics and relapse project/')
 load('./Datasets/metabolomics.relapse.v20180306.1.rdata')
 
-#' For reference: lists of the selected metabolites for each outcome.
-#' mrd.metabs <- c('pyruvate', "`cytidine 5'-diphosphocholine`", '`1-arachidonylglycerol (20:4)`')
-#' relapse.metabs <- c('`1-linoleoyl-GPI (18:2)*`', '`10-nonadecenoate (19:1n9)`', 'valine' , '`gamma-CEHC`')
-
 
 
 # MRD models --------------------------------------------------------------
@@ -23,11 +19,16 @@ load('./Datasets/metabolomics.relapse.v20180306.1.rdata')
 mrd.clin.model <- glm(mrd ~ nci.risk + immunophenotype + cyto.two.cat, data = subset(met, !is.na(met$mrd)), 
                       family = binomial(link = 'logit'))
 
-mrd.metab.model <- glm(mrd ~ pyruvate + `cytidine 5'-diphosphocholine` + `1-arachidonylglycerol (20:4)`,
+mrd.metab.model <- glm(mrd ~ pyruvate + `2-hydroxyoctanoate` + `1-arachidonylglycerol (20:4)`,
                        data = subset(met, !is.na(met$mrd)), family=binomial(link='logit'))
 
-mrd.combined.model <- glm(mrd ~ pyruvate + `cytidine 5'-diphosphocholine` + `1-arachidonylglycerol (20:4)` + nci.risk + immunophenotype + cyto.two.cat,
+#' Originally chose cytidine 5'-diphosphocholine as one of the metabolites. 
+#' Removed it because it was causing problems with linear separation,
+#' most likely to due to large number of samples with imputed values.
+#' Replacing with 2-hydroxyoctanoate solves problem, and produces better AIC.
+mrd.combined.model <- glm(mrd ~ pyruvate + `2-hydroxyoctanoate` + `1-arachidonylglycerol (20:4)` + nci.risk + immunophenotype + cyto.two.cat,
                           data = subset(met, !is.na(met$mrd)), family=binomial(link='logit'))
+mrd.backselect.model <- step(mrd.combined.model)
 
 #' Elements needed for ROC curve analysis.
 mrd.outcomes <- ifelse(as.numeric(mrd.clin.model$data$mrd)==2,1,0) #' Vector of outcomes will be the same for all 3 models.
@@ -35,15 +36,24 @@ mrd.clin.model.predprob <- mrd.clin.model$fitted.values
 mrd.metab.model.predprob <- mrd.metab.model$fitted.values
 mrd.combined.model.predprob <- mrd.combined.model$fitted.values
 
-mrd.models <- list(clin.model = list(model = mrd.clin.model, outcomes = mrd.outcomes, predicted.probabilities = mrd.clin.model.predprob),
-                   metab.model = list(model = mrd.metab.model, outcomes = mrd.outcomes, predicted.probabilities = mrd.metab.model.predprob),
-                   combined.model = list(model = mrd.combined.model, outcomes = mrd.outcomes, predicted.probabilities = mrd.combined.model.predprob))
+mrd.models <- list(clin.model = list(model = mrd.clin.model, outcomes = mrd.outcomes, 
+                                     predicted.probabilities = mrd.clin.model.predprob, 
+                                     backxform.coef = exp(cbind(mrd.clin.model$coefficients, confint(mrd.clin.model, level = 0.95)))),
+                   metab.model = list(model = mrd.metab.model, outcomes = mrd.outcomes, 
+                                      predicted.probabilities = mrd.metab.model.predprob,
+                                      backxform.coef = exp(cbind(mrd.metab.model$coefficients, confint(mrd.metab.model, level = 0.95)))),
+                   combined.model = list(model = mrd.combined.model, outcomes = mrd.outcomes, 
+                                         predicted.probabilities = mrd.combined.model.predprob,
+                                         backxform.coef = exp(cbind(mrd.combined.model$coefficients, confint(mrd.combined.model, level = 0.95)))),
+                   backseletion.model = list(model = mrd.backselect.model, outcomes = mrd.outcomes, 
+                                             predicted.probabilities = mrd.backselect.model$fitted.values,
+                                             backxform.coef = exp(cbind(mrd.backselect.model$coefficients, confint(mrd.backselect.model, level = 0.95)))))
 
 #' Methods for accessing stored elements of this list:
 #' tmp <- mrd.models$clin.model #' First nested list
 #' tmp$clin.model$predicted.probabilities #' Specified element of first nested list
 
-save(mrd.models, file = './Datasets/Expanded datasets/Logistic regression models/logreg.models.mrd.v20180326.1.rdata')
+save(mrd.models, file = './Datasets/Expanded datasets/Logistic regression models/logreg.models.mrd.v20180328.1.rdata')
 
 
 
@@ -57,16 +67,23 @@ rel.metab.model <- glm(relapse ~ `1-linoleoyl-GPI (18:2)*` + `10-nonadecenoate (
 
 rel.combined.model <- glm(relapse ~ `1-linoleoyl-GPI (18:2)*` + `10-nonadecenoate (19:1n9)` + valine + `gamma-CEHC` + nci.risk + immunophenotype + cyto.two.cat + mrd,
                           data = subset(met, !is.na(met$relapse) & !is.na(met$mrd)), family=binomial(link='logit'))
+rel.backselect.model <- step(rel.combined.model)
 
 rel.outcomes <- ifelse(as.numeric(rel.clin.model$data$relapse)==2,1,0) #' Vector of outcomes will be the same for all 3 models.
 rel.clin.model.predprob <- rel.clin.model$fitted.values
 rel.metab.model.predprob <- rel.metab.model$fitted.values
 rel.combined.model.predprob <- rel.combined.model$fitted.values
 
-rel.models <- list(clin.model = list(model = rel.clin.model, outcomes = rel.outcomes, predicted.probabilities = rel.clin.model.predprob),
-                   metab.model = list(model = rel.metab.model, outcomes = rel.outcomes, predicted.probabilities = rel.metab.model.predprob),
-                   combined.model = list(model = rel.combined.model, outcomes = rel.outcomes, predicted.probabilities = rel.combined.model.predprob))
+rel.models <- list(clin.model = list(model = rel.clin.model, outcomes = rel.outcomes, predicted.probabilities = rel.clin.model.predprob,
+                                     backxform.coef = exp(cbind(rel.clin.model$coefficients, confint(rel.clin.model, level = 0.95)))),
+                   metab.model = list(model = rel.metab.model, outcomes = rel.outcomes, predicted.probabilities = rel.metab.model.predprob,
+                                      backxform.coef = exp(cbind(rel.metab.model$coefficients, confint(rel.metab.model, level = 0.95)))),
+                   combined.model = list(model = rel.combined.model, outcomes = rel.outcomes, predicted.probabilities = rel.combined.model.predprob,
+                                         backxform.coef = exp(cbind(rel.combined.model$coefficients, confint(rel.combined.model, level = 0.95)))),
+                   backseletion.model = list(model = rel.backselect.model, outcomes = rel.outcomes, 
+                                             predicted.probabilities = rel.backselect.model$fitted.values,
+                                             backxform.coef = exp(cbind(rel.backselect.model$coefficients, confint(rel.backselect.model, level = 0.95)))))
 
-save(rel.models, file = './Datasets/Expanded datasets/Logistic regression models/logreg.models.relapse.v20180326.2.rdata')
+save(rel.models, file = './Datasets/Expanded datasets/Logistic regression models/logreg.models.relapse.v20180328.1.rdata')
 
 rm(list = ls()); gc()
