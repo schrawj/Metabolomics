b82fbb1cd203944391959aba4d169d45abadf55f
Jeremy Schraw <schraw@bcm.edu>
2018-04-20 10:50:09 -0500

    Updated metabolites, labels
    Includes p-values.  Behenate removed and appropriate lipid included in its place.

--- box plots for lipids and relapse.R
+++ box plots for lipids and relapse.R
@@ -15,60 +15,72 @@ par(mfrow=c(3,3),         #' Sets number of rows and columns
 
 boxplot(met$`arachidate (20:0)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Arachidate (20:0)',
+        main = 'Arachidate (20:0); p=0.01',
         ylim = c(0, 3))
 points(factor(met$relapse), met$`arachidate (20:0)`)
 
 
 boxplot(met$`linoleate (18:2n6)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Linoleate (18:2n6)',
+        main = 'Linoleate (18:2n6); p=0.01',
         ylim = c(0, 3))
 points(factor(met$relapse), met$`linoleate (18:2n6)`)
 
 
 boxplot(met$`linolenate [alpha or gamma; (18:3n3 or 6)]` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Linolenate (18:3n3 or n6)',
+        main = 'Linolenate (18:3n3 or n6); p=0.02',
         ylim = c(0, 5))
 points(factor(met$relapse), met$`linolenate [alpha or gamma; (18:3n3 or 6)]`)
 
 
 boxplot(met$`stearate (18:0)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Stearate (18:0)')
+        main = 'Stearate (18:0); p=0.02')
 points(factor(met$relapse), met$`stearate (18:0)`)
 
 
 boxplot(met$`dihomo-linoleate (20:2n6)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Dihomo-linoleate (20:2n6)',
+        main = 'Dihomo-linoleate (20:2n6); p=0.04',
         ylim = c(0, 5))
 points(factor(met$relapse), met$`dihomo-linoleate (20:2n6)`)
 
 
 boxplot(met$`palmitate (16:0)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Palmitate (16:0)')
+        main = 'Palmitate (16:0), p=0.03')
 points(factor(met$relapse), met$`palmitate (16:0)`)
 
 
 boxplot(met$`oleate/vaccenate (18:1)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Oleate/Vaccenate (18:1)')
+        main = 'Oleate/Vaccenate (18:1); p=0.04')
 points(factor(met$relapse), met$`oleate/vaccenate (18:1)`)
 
 
 boxplot(met$`eicosenoate (20:1)` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Eicosanoate (20:1)',
+        main = 'Eicosanoate (20:1); p=0.04',
         ylim = c(0, 5))
 points(factor(met$relapse), met$`eicosenoate (20:1)`)
 
 
+boxplot(met$`dihomo-linolenate (20:3n3 or n6)` ~ met$relapse, 
+        col = c('gold', 'darkgreen'), 
+        main = 'Dihomo-linolenate (20:3n3 or n6); p=0.04',
+        ylim = c(0, 3))
+points(factor(met$relapse), met$`dihomo-linolenate (20:3n3 or n6)`)
+
+
+
+
+
+
+
 boxplot(met$`behenate (22:0)*` ~ met$relapse, 
         col = c('gold', 'darkgreen'), 
-        main = 'Behenate (22:0)',
+        main = 'Behenate (22:0); p=0.05',
         ylim = c(0, 4))
 points(factor(met$relapse), met$`behenate (22:0)*`)
 
