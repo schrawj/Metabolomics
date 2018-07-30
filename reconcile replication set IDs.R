4bc286a978b921f9d28414be76ce112e7a9b1eb9
Jeremy Schraw <schraw@bcm.edu>
2018-07-30 12:44:14 -0500

    Additional filtering
    Inlcudes additional filtering based on manual review of patient records to exclude AML patients and some who transferred in or out of TCH care.

--- reconcile replication set IDs.R
+++ reconcile replication set IDs.R
@@ -21,14 +21,12 @@
 #'-------------------------------------------------------------------------
 
 #'TODO: Remove children originally DX'd with any condition than ALL.
-#'TODO: Return curated list to Karen, Olga, Theresa Ty.
-
 
 
 
 # Read in patient lists, remove duplicates, write joined list -------------
 
-require(xlsx); require(dplyr)
+require(xlsx); require(dplyr); require(stringr)
 
 karen.list <- read.xlsx(file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/additional patients 6-17-18 with vials available.xlsx',
                         sheetName = "Rabin's result_1_3_18", colIndex = 1:9, rowIndex = 1:58, header = TRUE, stringsAsFactors = FALSE)
@@ -41,7 +39,7 @@ olga.list.relapse <- read.xlsx(file = 'Y:/Jeremy Schraw/Metabolomics and relapse
 
 load('Y:/Jeremy Schraw/Metabolomics and relapse project/Datasets/Expanded datasets/metabolomics.subjects.expanded.identifiers.rdata')
 
-olga.names <- c('unknown','mrn','rtss','unknown2','last.name','first.name','unknown3','unknown.date','mrd','relapse')
+olga.names <- c('unknown','mrn','rtss','unknown2','last.name','first.name','unknown3','dob','mrd','relapse')
 
 names(metab.clin.expanded.ids) <- tolower(names(metab.clin.expanded.ids))
 names(olga.list.mrd) <- olga.names
@@ -63,14 +61,87 @@ olga.dups <- semi_join(olga.list.mrd, olga.list.relapse, 'mrn')
 olga.list.mrd <- anti_join(olga.list.mrd, olga.list.relapse, 'mrn') 
 olga.list <- arrange(rbind(olga.list.mrd, olga.list.relapse), last.name)
 
+#' Export Olga's list to Excel for some manual annotation.
+#' Will fill in a more standardized variable for MRD, and flag cases who might not be suitable:
+#' MRD not measured on day 29, relapsed after BMT, transferred care, AML or Burkitt lymphoma
+write.xlsx(olga.list, file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/MRD+ Relapse_jms.xlsx', row.names = FALSE)
+
+#' Read the edited version back in, remove unnecessary columns and clean up relapse data.
+olga.list <- read.xlsx(file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/MRD+ Relapse_jms_edited.xlsx',
+                       stringsAsFactors = FALSE, sheetIndex = 1)
+olga.list <- select(filter(olga.list, exclude.flag != 1), -exclude.flag, -unknown3)
+olga.list$mrd.status <- paste('mrd',olga.list$mrd.status)
+olga.list$relapse <- str_trim(olga.list$relapse, side = 'both')
+olga.list$relapse <- tolower(olga.list$relapse)
+olga.list$relapse.status <- ifelse(olga.list$relapse == 'no relapse', "no relapse", "relapse")
+olga.list$group <- paste(paste(olga.list$mrd.status, olga.list$relapse.status))
+
+combinations <- unique(paste(olga.list$mrd.status, olga.list$relapse.status))
+
+for (i in 1:length(combinations)){
+  
+  name <- combinations[i]
+  
+  tmp <- select(filter(olga.list, group == combinations[i]), -group)
+  
+  write.xlsx(tmp, file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/replication.set.potential.ids.xlsx',
+             row.names = FALSE, append = TRUE, sheetName = name)
+}
+
+rm(list = ls()); gc()
+
+
+
+# Further checks to Olga's list -------------------------------------------
+
+#'-------------------------------------------------------------------------
+#'-------------------------------------------------------------------------
+#' 2018.07.27.
+#' 
+#' Per email exchange with Karen on 7/25, some of the patients in this list
+#' are AML.  Need to further check them to confirm their original DX was 
+#' ALL, make sure that % MRD is for day 29, and that we retrieve date the 
+#' date of their original diagnosis.
+#' 
+#' As of today, have combed through EPIC to get this data and there were 
+#' indeed 11 AML patients and 1 biphenotypic (T/myeloid) patient.  Also a 
+#' few patients who did not undergo all of their therapy at TXCH.
+#' 
+#' Made a command decision to exclude 1 who was lost to follow-up.  Will 
+#' hold off on excluding patients who did not have their initial therapy
+#' at TXCH even though their D29 marrows will not be available. 
+#' 
+#' Import verified data and sort by MRD and relapse status.
+#'-------------------------------------------------------------------------
+#'-------------------------------------------------------------------------
+
+require(xlsx); require(dplyr)
+
+#' Read in verified data.
+curated.list <- read.xlsx('Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/replication.set.potential.ids.epic.check.xlsx',
+                          header = TRUE, stringsAsFactors = FALSE, sheetName = 'received')
+
+#' Exclude AML and biphenotypic patients - also 1 patient lost to follow-up.
+curated.list <- select(filter(curated.list, exclude.flag != 1), -exclude.flag)
+
+write.xlsx(curated.list, file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/replication.set.potential.ids.epic.check.xlsx',
+           row.names = FALSE, append = TRUE, sheetName = 'final', showNA = FALSE)
+
+rm(list = ls()); gc()
+
+# Scratch paper -----------------------------------------------------------
+
 olga.dups <- semi_join(olga.list, karen.list, 'mrn')
 olga.list <- anti_join(olga.list, karen.list, 'mrn')
 
+#' Create a common set of informative variables.
 olga.list$source.list <- 'Olga'
 karen.list$source.list <- 'Karen'
+karen.list$relapse <- karen.list$cohort
 
-joined.list <- rbind(select(karen.list, mrn, last.name, first.name, source.list),
-                     select(olga.list, mrn, last.name, first.name, source.list))
+joined.list <- rbind(select(karen.list, mrn, last.name, first.name, source.list, mrd, relapse),
+                     select(olga.list, mrn, last.name, first.name, source.list, mrd, relapse))
+joined.list$checked.with.rtss <- ifelse(joined.list$source.list == 'Karen', 'Yes', "No")
 
 write.xlsx(joined.list, file = 'Y:/Jeremy Schraw/Metabolomics and relapse project/Replication set/olga.and.karen.lists.join.xlsx', row.names = FALSE)
 
